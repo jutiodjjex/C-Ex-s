169f100a4246e4bc5f030111db5de816525d30a1
jutiodjjex <40070146+jutiodjjex@users.noreply.github.com>
2019-09-26 05:19:26 +0300

    ex 2 added

---
+++ Ex2/ex2Solution.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <random>
+
+
+int getScore(int getShotRange){
+    
+    switch(getShotRange){
+        case 1:
+        return 10;
+        break;
+        
+        case 2:
+        return 8;
+        break;
+        
+        case 3:
+        return 6;
+        break;
+        
+        case 4:
+        return 4;
+        break;
+        
+        case 5:
+        return 2;
+        break;
+        
+        default:
+        break;
+    }
+    
+}
+
+int main()
+{
+    
+    int summaryScore = 0; //Инициализируем изначальные баллы
+    std::random_device r; //Берём независимый seed из железа компьютера
+    
+    // Выбираем случайное число от 1 до 6, потому что у нас всего 5 кругов в мишени, а 6 - не попал.
+    std::default_random_engine targetCircles(r()); //Выбираем стандартный рандомайзер
+    std::uniform_int_distribution<int> uniform_dist(1, 6); //Выбираем рендж цифр, в данном случае от 1 до 6
+    
+    for(int i = 1; i < 4; i++){
+    int tempShotScore = getScore(uniform_dist(targetCircles));
+    summaryScore += tempShotScore;
+    std::cout << "Score for " << i << " shot is: " << tempShotScore << std::endl;
+    
+    }
+    
+    //Определяем начисление кол-ва очков за попадание
+    
+    std::cout << "Your score: " << summaryScore << std::endl;
+    
+    return 0;
+}
